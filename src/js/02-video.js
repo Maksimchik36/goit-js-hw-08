1a0ffe4e763cba43b3f94a00a0aeac99043a21b6
Maksimchik36 <Peltek1985@gmail.com>
2022-05-09 23:18:43 +0300

    add part of task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+// import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('play', function(timeupdate) {
+    localStorage.setItem(LOCALSTORAGE_KEY, timeupdate)
+});
+
+
+
 
