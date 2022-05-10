104b973f903a2d28cd290d541c948d0e694bedc9
Maksimchik36 <Peltek1985@gmail.com>
2022-05-10 13:47:10 +0300

    task 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,28 @@
-// import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
-player.on('play', function(timeupdate) {
-    localStorage.setItem(LOCALSTORAGE_KEY, timeupdate)
+const updateTime = function (timeupdate) {
+    localStorage.setItem(LOCALSTORAGE_KEY, timeupdate.seconds);
+}
+
+player.on('timeupdate', throttle(updateTime, 1000));
+
+//  _.throttle(func, [wait=0], [options={}])
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY)).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
-
-
-
 
