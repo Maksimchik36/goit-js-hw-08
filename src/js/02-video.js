78cc13634f0a38e56bee0453c33bc0261de6aa3c
Maksimchik36 <Peltek1985@gmail.com>
2022-05-10 22:23:52 +0300

    null verification was added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,15 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
+// Записывает текущий хронометраж видео в localeStorage
 const writeValueToLocaleStorage = function (timeupdate) {
     localStorage.setItem(LOCALSTORAGE_KEY, timeupdate.seconds);
 }
 
+// При проигрывании выполняет ф-ю writeValueToLocaleStorage с интервалом в 1 сек
 player.on('timeupdate', throttle(writeValueToLocaleStorage, 1000));
 
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+const saveTime = localStorage.getItem(LOCALSTORAGE_KEY);
+//Для первоначальной записи нужно проверить, чтобы значение было не null
+if(saveTime){
+player.setCurrentTime(saveTime)}
